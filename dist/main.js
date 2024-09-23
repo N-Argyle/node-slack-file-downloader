e7a8fed4a31dd3e7f48282d7ba1c88a6df873ef0
Nick Argyle <nickargyle@protonmail.com>
2024-09-23 11:07:19 -0400

    Improved logging

--- dist/main.js
+++ dist/main.js
@@ -6,10 +6,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const glob_promise_1 = __importDefault(require("glob-promise"));
+const fs_1 = __importDefault(require("fs"));
 const cli_1 = require("./cli");
 const parser_1 = require("./parser");
 const download_1 = require("./download");
 async function main() {
+    const packageJson = JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, '../package.json'), 'utf-8'));
+    const version = packageJson.version;
     const jsonFiles = await (0, glob_promise_1.default)(path_1.default.join(cli_1.argv.dir, '**', '*.json'), {
         ignore: [
             '**/node_modules/**',
@@ -18,6 +21,8 @@ async function main() {
             '**/*.schema.json'
         ]
     });
+    console.log(`\x1b[1mSlack File Downloader v${version}\x1b[0m`);
+    console.log(`More info at \x1b[4mhttps://github.com/N-Argyle/node-slack-file-downloader\x1b[0m`);
     console.log(`Found ${jsonFiles.length} JSON files.`);
     console.log('Parsing JSON files...');
     const downloadTasks = await (0, parser_1.parseJsonFiles)(jsonFiles);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import path from 'path';
 import glob from 'glob-promise';
+import fs from 'fs';
 import { argv } from './cli';
 import { parseJsonFiles } from './parser';
 import { downloadFiles } from './download';
 
 async function main(): Promise<void> {
+  const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'));
+  const version = packageJson.version;
+
   const jsonFiles = await glob(path.join(argv.dir as string, '**', '*.json'), {
     ignore: [
       '**/node_modules/**',
@@ -14,6 +18,10 @@ async function main(): Promise<void> {
       '**/*.schema.json'
     ]
   });
+  console.log(`\x1b[1mSlack File Downloader v${version}\x1b[0m`);
+
+  console.log(`More info at \x1b[4mhttps://github.com/N-Argyle/node-slack-file-downloader\x1b[0m`);
+  
   console.log(`Found ${jsonFiles.length} JSON files.`);
 
   console.log('Parsing JSON files...');
